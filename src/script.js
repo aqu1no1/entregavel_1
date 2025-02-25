cdcb2b3d74bf73fc3d542bd3f61de0911584541a
Mauricio Aquino <aquinomauricio101@gmail.com>
2025-02-25 16:22:58 -0300

    Codigo da calculadora

--- src/script.js
+++ src/script.js
@@ -2,7 +2,41 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(num1,num2,operacao){
+  let resultado = 0
+  num1 = Number(num1)
+  num2 = Number(num2)
 
+   if(operacao === "+"){
+          resultado = num1 + num2 
+          return  resultado;
+
+   } else if (operacao === "-") {
+          resultado = num1 - num2 
+         return  resultado;
+
+   } else if (operacao === "*") {
+          resultado = num1 * num2
+          return  resultado;
+
+   } else if (operacao === "/") {
+          resultado = num1 / num2
+          return  resultado;
+    
+   } else if (operacao === "e") { 
+          resultado = 1;  
+          for(let i = 0; i < num2;i++){
+              resultado *= num1
+          }
+          // outro jeito de fazer resultado = num1 ** num2
+          return  resultado;
+
+   } else if (resultado === "undefined" || resultado > 1000000) {
+          resultado = "ERRO"
+
+   } 
+
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
